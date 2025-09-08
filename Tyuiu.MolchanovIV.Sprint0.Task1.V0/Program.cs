819d872f8b3562fd1bff69fe21c8f824100775c9
MolchanovIV <Tyuiu.MolchanovIV@outlook.com>
2025-09-09 00:16:44 +0500

    Task1

---
+++ Tyuiu.MolchanovIV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MolchanovIV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Иван";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год!");
+            Console.ReadKey();
+        }
+    }
+}
