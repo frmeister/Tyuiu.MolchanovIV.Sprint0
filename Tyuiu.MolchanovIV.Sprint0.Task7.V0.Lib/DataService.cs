f2cd31496bcf0d4ef3bcb745be49e8565458053e
MolchanovIV <Tyuiu.MolchanovIV@outlook.com>
2025-09-11 13:23:37 +0500

    Task7

---
+++ Tyuiu.MolchanovIV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MolchanovIV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+
+        // Пример циклической структуры (цикл с параметрами) for
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+
+            for(var i = 0; i < resultArray.Length; i++)
+            {
+
+                resultArray[i] = numOne[i] + numTwo[i];
+
+            }
+
+            return resultArray;
+        }
+    }
+}
